66779763c9adf081d180419b618f20d1d0973de3
Aree-Vanier <git@gregk.ca>
2020-01-19 19:01:59 -0500

    Added header and util php files

--- public/index.php
+++ public/index.php
@@ -1,13 +1,15 @@
 <head>
-	<title>Landing Page</title>
+    <title>Landing Page</title>
 </head>
 
-<body>	
-	<?php 
-	$var = "Hello World!";
-	
-	echo "<h1>$var</h1>";
-	
-	echo "This is a demonstration page"?>
-	
+<body>
+<?php
+include ($_SERVER["DOCUMENT_ROOT"]."/public/shared/header.php");
+
+$var = "Hello World!";
+
+echo "<h1>$var</h1>";
+
+echo "This is a demonstration page"?>
+
 </body>
---
+++ public/shared/header.php
@@ -0,0 +1,9 @@
+<?php
+    include($_SERVER["DOCUMENT_ROOT"]."/backend/utils.php")
+?>
+
+<header style="background-color:blue">
+    <h1 style="color:white">Header</h1>
+
+
+</header>
