12881c6ea17f14a19b94d74f71e0567d2e13f8d1
Aree-Vanier <git@gregk.ca>
2020-01-20 11:31:19 -0500

    Started reconstructing with custom CSS

--- public/shared/header.php
+++ public/shared/header.php
@@ -5,7 +5,6 @@
     <h1>Header</h1>
 
 </header>
-
 <nav>
     <ul>
         <li class="nav-item">
@@ -19,13 +18,6 @@
         </li>
         <li class="nav-item">
             <a class="nav-link disabled" href="#">Page 4</a>
-        </li>
-        <li class="nav-item right">
-
-            <form class="form-inline">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
         </li>
     </ul>
 </nav>
