9afc05e486a7bca7a2c8da96455650387c6e6866
aree-vanier <gregory@ajayinkingston.com>
2020-02-01 23:14:43 -0500

    Added simple UI for joining queue

--- public/queue/join.php
+++ public/queue/join.php
@@ -4,14 +4,39 @@
 <head>
     <title>Landing Page</title>
     <?php include(META) ?>
+
 </head>
 
 <body>
 <?php
 include(HEADER); ?>
 <section>
-
-
+    <div style="text-align: center;">
+        <h2>Estimated Wait Time</h2>
+        <h1 style="font-size: 5em; margin:0">1:00</h1>
+        <h3>30 people in queue</h3>
+    </div>
+</section>
+<section>
+    <h1>Join Queue</h1>
+    <form>
+        <table>
+            <tr>
+                <td>Name:</td>
+                <td><input id="name" type="text" placeholder="name" required/></td>
+            </tr>
+            <tr>
+                <td>Email</td>
+                <td><input id="email" type="email" placeholder="mail@example.com"/></td>
+            </tr>
+            <tr>
+                <td>Phone #</td>
+                <td><input id="phone" type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" oninput="formatNumber(this)"/>
+                </td>
+            </tr>
+        </table>
+        <input type="submit" value="Join">
+    </form>
 </section>
 </body>
 </html>
