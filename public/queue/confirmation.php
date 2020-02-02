2965f7210cd3011b6abefd61750b977fa2bd5620
aree-vanier <gregory@ajayinkingston.com>
2020-02-01 23:44:20 -0500

    Made skeleton confirmation frontend

---
+++ public/queue/confirmation.php
@@ -0,0 +1,23 @@
+<?php include($_SERVER["DOCUMENT_ROOT"] . "/backend/utils.php") ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Landing Page</title>
+    <?php include(META) ?>
+    <link rel="stylesheet" href="/styles/header.css">
+</head>
+
+<body>
+    <header style="text-align: center;">
+        <img src="/images/logo.svg">
+        <h1>You're in queue!</h1>
+    </header>
+    <section style="text-align: center;">
+        <h2>Your number</h2>
+        <h1 style="font-size: 5em; margin:0">80</h1>
+        <h2>Code: 9afc05e486</h2>
+
+
+    </section>
+</body>
+</html>
