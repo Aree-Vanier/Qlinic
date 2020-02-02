ba164ab127860fc32814f8faa1f1459c6a62fee8
aree-vanier <gregory@ajayinkingston.com>
2020-02-01 22:15:35 -0500

    Updated .htaccess to automatically add .php extension

---
+++ public/queue/join.php
@@ -0,0 +1,17 @@
+<?php include($_SERVER["DOCUMENT_ROOT"] . "/backend/utils.php") ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Landing Page</title>
+    <?php include(META) ?>
+</head>
+
+<body>
+<?php
+include(HEADER); ?>
+<section>
+
+
+</section>
+</body>
+</html>
