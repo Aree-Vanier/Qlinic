0802e932e4be1ba5fd98dfd0d2b9e1fdcdbeb732
Aree-Vanier <git@gregk.ca>
2020-02-10 23:47:35 -0500

    Started work on appointment frontend. Added spaces for calendar and angenda

--- public/RIO/rio.php
+++ public/RIO/rio.php
@@ -13,103 +13,141 @@
     <img src="/public/images/logo.svg"/><br/>
     Receptionist Interface
 </header>
-<section id="queue">
-    <h1>Queue</h1>
-    <button>Serve Next</button><br/>
-    <div class="scroller">
-        <input class="scrollerInput" name="time" type="hidden" value="">
-        <div class="scrollItem current" id="1100">
-            <span class="scrollTitle">Gregory Kelly</span>
-            <span class="scrollInfo position">#30</span><br/>
-            <span class="scrollTitle">QWERTYT</span>
+<div id="container">
+    <section id="queue">
+        <h1>Queue</h1>
+        <button>Serve Next</button>
+        <br/>
+        <div class="scroller">
+            <input class="scrollerInput" name="time" type="hidden" value="">
+            <div class="scrollItem current" id="1100">
+                <span class="scrollTitle">Gregory Kelly</span>
+                <span class="scrollInfo position">#30</span><br/>
+                <span class="scrollTitle">QWERTYT</span>
+            </div>
+            <div class="scrollItem next" id="1100">
+                <span class="scrollTitle">Gregory Kelly</span>
+                <span class="scrollInfo position">#30</span><br/>
+                <span class="scrollTitle">QWERTYT</span>
+            </div>
+            <div class="scrollItem" id="1100">
+                <span class="scrollTitle">Gregory Kelly</span>
+                <span class="scrollInfo position">#30</span><br/>
+                <span class="scrollTitle">QWERTYT</span>
+            </div>
+            <div class="scrollItem" id="1100">
+                <span class="scrollTitle">Gregory Kelly</span>
+                <span class="scrollInfo position">#30</span><br/>
+                <span class="scrollTitle">QWERTYT</span>
+            </div>
+            <div class="scrollItem" id="1100">
+                <span class="scrollTitle">Gregory Kelly</span>
+                <span class="scrollInfo position">#30</span><br/>
+                <span class="scrollTitle">QWERTYT</span>
+            </div>
+            <div class="scrollItem" id="1100">
+                <span class="scrollTitle">Gregory Kelly</span>
+                <span class="scrollInfo position">#30</span><br/>
+                <span class="scrollTitle">QWERTYT</span>
+            </div>
+            <div class="scrollItem" id="1100">
+                <span class="scrollTitle">Gregory Kelly</span>
+                <span class="scrollInfo position">#30</span><br/>
+                <span class="scrollTitle">QWERTYT</span>
+            </div>
+            <div class="scrollItem" id="1100">
+                <span class="scrollTitle">Gregory Kelly</span>
+                <span class="scrollInfo position">#30</span><br/>
+                <span class="scrollTitle">QWERTYT</span>
+            </div>
+            <div class="scrollItem" id="1100">
+                <span class="scrollTitle">Gregory Kelly</span>
+                <span class="scrollInfo position">#30</span><br/>
+                <span class="scrollTitle">QWERTYT</span>
+            </div>
+            <div class="scrollItem" id="1100">
+                <span class="scrollTitle">Gregory Kelly</span>
+                <span class="scrollInfo position">#30</span><br/>
+                <span class="scrollTitle">QWERTYT</span>
+            </div>
+            <div class="scrollItem" id="1100">
+                <span class="scrollTitle">Gregory Kelly</span>
+                <span class="scrollInfo position">#30</span><br/>
+                <span class="scrollTitle">QWERTYT</span>
+            </div>
+            <div class="scrollItem" id="1100">
+                <span class="scrollTitle">Gregory Kelly</span>
+                <span class="scrollInfo position">#30</span><br/>
+                <span class="scrollTitle">QWERTYT</span>
+            </div>
+            <div class="scrollItem" id="1100">
+                <span class="scrollTitle">Gregory Kelly</span>
+                <span class="scrollInfo position">#30</span><br/>
+                <span class="scrollTitle">QWERTYT</span>
+            </div>
+            <div class="scrollItem" id="1100">
+                <span class="scrollTitle">Gregory Kelly</span>
+                <span class="scrollInfo position">#30</span><br/>
+                <span class="scrollTitle">QWERTYT</span>
+            </div>
+            <div class="scrollItem" id="1100">
+                <span class="scrollTitle">Gregory Kelly</span>
+                <span class="scrollInfo position">#30</span><br/>
+                <span class="scrollTitle">QWERTYT</span>
+            </div>
+            <div class="scrollItem" id="1100">
+                <span class="scrollTitle">Gregory Kelly</span>
+                <span class="scrollInfo position">#30</span><br/>
+                <span class="scrollTitle">QWERTYT</span>
+            </div>
+            <div class="scrollItem" id="1100">
+                <span class="scrollTitle">Gregory Kelly</span>
+                <span class="scrollInfo position">#30</span><br/>
+                <span class="scrollTitle">QWERTYT</span>
+            </div>
+            <div class="scrollItem" id="1100">
+                <span class="scrollTitle">Gregory Kelly</span>
+                <span class="scrollInfo position">#30</span><br/>
+                <span class="scrollTitle">QWERTYT</span>
+            </div>
+            <div class="scrollItem" id="1100">
+                <span class="scrollTitle">Gregory Kelly</span>
+                <span class="scrollInfo position">#30</span><br/>
+                <span class="scrollTitle">QWERTYT</span>
+            </div>
+            <div class="scrollItem" id="1100">
+                <span class="scrollTitle">Gregory Kelly</span>
+                <span class="scrollInfo position">#30</span><br/>
+                <span class="scrollTitle">QWERTYT</span>
+            </div>
+            <div class="scrollItem" id="1100">
+                <span class="scrollTitle">Gregory Kelly</span>
+                <span class="scrollInfo position">#30</span><br/>
+                <span class="scrollTitle">QWERTYT</span>
+            </div>
         </div>
-        <div class="scrollItem next" id="1100">
-            <span class="scrollTitle">Gregory Kelly</span>
-            <span class="scrollInfo position">#30</span><br/>
-            <span class="scrollTitle">QWERTYT</span>
-        </div>        <div class="scrollItem" id="1100">
-            <span class="scrollTitle">Gregory Kelly</span>
-            <span class="scrollInfo position">#30</span><br/>
-            <span class="scrollTitle">QWERTYT</span>
-        </div>        <div class="scrollItem" id="1100">
-            <span class="scrollTitle">Gregory Kelly</span>
-            <span class="scrollInfo position">#30</span><br/>
-            <span class="scrollTitle">QWERTYT</span>
-        </div>        <div class="scrollItem" id="1100">
-            <span class="scrollTitle">Gregory Kelly</span>
-            <span class="scrollInfo position">#30</span><br/>
-            <span class="scrollTitle">QWERTYT</span>
-        </div>        <div class="scrollItem" id="1100">
-            <span class="scrollTitle">Gregory Kelly</span>
-            <span class="scrollInfo position">#30</span><br/>
-            <span class="scrollTitle">QWERTYT</span>
-        </div>        <div class="scrollItem" id="1100">
-            <span class="scrollTitle">Gregory Kelly</span>
-            <span class="scrollInfo position">#30</span><br/>
-            <span class="scrollTitle">QWERTYT</span>
-        </div>        <div class="scrollItem" id="1100">
-            <span class="scrollTitle">Gregory Kelly</span>
-            <span class="scrollInfo position">#30</span><br/>
-            <span class="scrollTitle">QWERTYT</span>
-        </div>        <div class="scrollItem" id="1100">
-            <span class="scrollTitle">Gregory Kelly</span>
-            <span class="scrollInfo position">#30</span><br/>
-            <span class="scrollTitle">QWERTYT</span>
-        </div>        <div class="scrollItem" id="1100">
-            <span class="scrollTitle">Gregory Kelly</span>
-            <span class="scrollInfo position">#30</span><br/>
-            <span class="scrollTitle">QWERTYT</span>
-        </div>        <div class="scrollItem" id="1100">
-            <span class="scrollTitle">Gregory Kelly</span>
-            <span class="scrollInfo position">#30</span><br/>
-            <span class="scrollTitle">QWERTYT</span>
-        </div>        <div class="scrollItem" id="1100">
-            <span class="scrollTitle">Gregory Kelly</span>
-            <span class="scrollInfo position">#30</span><br/>
-            <span class="scrollTitle">QWERTYT</span>
-        </div>        <div class="scrollItem" id="1100">
-            <span class="scrollTitle">Gregory Kelly</span>
-            <span class="scrollInfo position">#30</span><br/>
-            <span class="scrollTitle">QWERTYT</span>
-        </div>        <div class="scrollItem" id="1100">
-            <span class="scrollTitle">Gregory Kelly</span>
-            <span class="scrollInfo position">#30</span><br/>
-            <span class="scrollTitle">QWERTYT</span>
-        </div>        <div class="scrollItem" id="1100">
-            <span class="scrollTitle">Gregory Kelly</span>
-            <span class="scrollInfo position">#30</span><br/>
-            <span class="scrollTitle">QWERTYT</span>
-        </div>        <div class="scrollItem" id="1100">
-            <span class="scrollTitle">Gregory Kelly</span>
-            <span class="scrollInfo position">#30</span><br/>
-            <span class="scrollTitle">QWERTYT</span>
-        </div>        <div class="scrollItem" id="1100">
-            <span class="scrollTitle">Gregory Kelly</span>
-            <span class="scrollInfo position">#30</span><br/>
-            <span class="scrollTitle">QWERTYT</span>
-        </div>        <div class="scrollItem" id="1100">
-            <span class="scrollTitle">Gregory Kelly</span>
-            <span class="scrollInfo position">#30</span><br/>
-            <span class="scrollTitle">QWERTYT</span>
-        </div>        <div class="scrollItem" id="1100">
-            <span class="scrollTitle">Gregory Kelly</span>
-            <span class="scrollInfo position">#30</span><br/>
-            <span class="scrollTitle">QWERTYT</span>
-        </div>        <div class="scrollItem" id="1100">
-            <span class="scrollTitle">Gregory Kelly</span>
-            <span class="scrollInfo position">#30</span><br/>
-            <span class="scrollTitle">QWERTYT</span>
-        </div>        <div class="scrollItem" id="1100">
-            <span class="scrollTitle">Gregory Kelly</span>
-            <span class="scrollInfo position">#30</span><br/>
-            <span class="scrollTitle">QWERTYT</span>
+        <div id="buttons">
+            <button>Add to queue</button>
+            <button>Remove from queue</button>
         </div>
-    </div>
-    <div id="buttons">
-        <button>Add to queue</button>
-        <button>Remove from queue</button>
-    </div>
+    </section>
+    <section id="appointments">
+        <div>
+            <h1>Appointments</h1>
+            <button>Book Appointment</button>
+            <button>Find Appointment</button>
+        </div>
+        <div class="agenda">
+            <h2>Today's Appointments</h2>
+            <div class="scroller">
 
-</section>
+            </div>
+        </div>
+        <div class="calendar">
+            <h2>Two Weeks</h2>
+
+        </div>
+    </section>
+</div>
 </body>
 </html>
