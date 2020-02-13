f38906c546d12aa94b12f888faa97bab5505b935
Aree-Vanier <git@gregk.ca>
2020-02-13 11:25:41 -0500

    Generated fake names for the queue

--- public/RIO/rio.php
+++ public/RIO/rio.php
@@ -21,110 +21,95 @@
         <br/>
         <div class="scroller">
             <input class="scrollerInput" name="time" type="hidden" value="">
-            <div class="scrollItem current" id="1100">
-                <span class="scrollTitle">Gregory Kelly</span>
-                <span class="scrollInfo position">#30</span><br/>
-                <span class="scrollTitle">QWERTYT</span>
+            <div class="scrollItem current" id="">
+                <span class="scrollTitle">Gavin Estevez</span>
+                <span class="scrollInfo position">#16</span><br/>
+                <span class="scrollTitle">OJCPL</span>
             </div>
-            <div class="scrollItem next" id="1100">
-                <span class="scrollTitle">Gregory Kelly</span>
-                <span class="scrollInfo position">#30</span><br/>
-                <span class="scrollTitle">QWERTYT</span>
+            <div class="scrollItem next" id="">
+                <span class="scrollTitle">Ned Everette</span>
+                <span class="scrollInfo position">#17</span><br/>
+                <span class="scrollTitle">QFAUF</span>
             </div>
-            <div class="scrollItem" id="1100">
-                <span class="scrollTitle">Gregory Kelly</span>
-                <span class="scrollInfo position">#30</span><br/>
-                <span class="scrollTitle">QWERTYT</span>
+            <div class="scrollItem" id="">
+                <span class="scrollTitle">Bud Wedell</span>
+                <span class="scrollInfo position">#18</span><br/>
+                <span class="scrollTitle">JIKLO</span>
             </div>
-            <div class="scrollItem" id="1100">
-                <span class="scrollTitle">Gregory Kelly</span>
-                <span class="scrollInfo position">#30</span><br/>
-                <span class="scrollTitle">QWERTYT</span>
+            <div class="scrollItem" id="">
+                <span class="scrollTitle">Whitney Sapp</span>
+                <span class="scrollInfo position">#19</span><br/>
+                <span class="scrollTitle">ZOSML</span>
             </div>
-            <div class="scrollItem" id="1100">
-                <span class="scrollTitle">Gregory Kelly</span>
-                <span class="scrollInfo position">#30</span><br/>
-                <span class="scrollTitle">QWERTYT</span>
+            <div class="scrollItem" id="">
+                <span class="scrollTitle">Shon Pitzer</span>
+                <span class="scrollInfo position">#20</span><br/>
+                <span class="scrollTitle">OJIII</span>
             </div>
-            <div class="scrollItem" id="1100">
-                <span class="scrollTitle">Gregory Kelly</span>
-                <span class="scrollInfo position">#30</span><br/>
-                <span class="scrollTitle">QWERTYT</span>
+            <div class="scrollItem" id="">
+                <span class="scrollTitle">Korey Winchenbach</span>
+                <span class="scrollInfo position">#21</span><br/>
+                <span class="scrollTitle">KHMON</span>
             </div>
-            <div class="scrollItem" id="1100">
-                <span class="scrollTitle">Gregory Kelly</span>
-                <span class="scrollInfo position">#30</span><br/>
-                <span class="scrollTitle">QWERTYT</span>
+            <div class="scrollItem" id="">
+                <span class="scrollTitle">Chi Mount</span>
+                <span class="scrollInfo position">#22</span><br/>
+                <span class="scrollTitle">WUPIO</span>
             </div>
-            <div class="scrollItem" id="1100">
-                <span class="scrollTitle">Gregory Kelly</span>
-                <span class="scrollInfo position">#30</span><br/>
-                <span class="scrollTitle">QWERTYT</span>
+            <div class="scrollItem" id="">
+                <span class="scrollTitle">Sung Tygart</span>
+                <span class="scrollInfo position">#23</span><br/>
+                <span class="scrollTitle">UDVDW</span>
             </div>
-            <div class="scrollItem" id="1100">
-                <span class="scrollTitle">Gregory Kelly</span>
-                <span class="scrollInfo position">#30</span><br/>
-                <span class="scrollTitle">QWERTYT</span>
+            <div class="scrollItem" id="">
+                <span class="scrollTitle">Donnie Oliverio</span>
+                <span class="scrollInfo position">#24</span><br/>
+                <span class="scrollTitle">CBKUL</span>
             </div>
-            <div class="scrollItem" id="1100">
-                <span class="scrollTitle">Gregory Kelly</span>
-                <span class="scrollInfo position">#30</span><br/>
-                <span class="scrollTitle">QWERTYT</span>
+            <div class="scrollItem" id="">
+                <span class="scrollTitle">Tomas Ptak</span>
+                <span class="scrollInfo position">#25</span><br/>
+                <span class="scrollTitle">ARLGC</span>
             </div>
-            <div class="scrollItem" id="1100">
-                <span class="scrollTitle">Gregory Kelly</span>
-                <span class="scrollInfo position">#30</span><br/>
-                <span class="scrollTitle">QWERTYT</span>
+            <div class="scrollItem" id="">
+                <span class="scrollTitle">David Marcotte</span>
+                <span class="scrollInfo position">#26</span><br/>
+                <span class="scrollTitle">RPMFM</span>
             </div>
-            <div class="scrollItem" id="1100">
-                <span class="scrollTitle">Gregory Kelly</span>
-                <span class="scrollInfo position">#30</span><br/>
-                <span class="scrollTitle">QWERTYT</span>
+            <div class="scrollItem" id="">
+                <span class="scrollTitle">Shirleen Bruch</span>
+                <span class="scrollInfo position">#27</span><br/>
+                <span class="scrollTitle">VCFOL</span>
             </div>
-            <div class="scrollItem" id="1100">
-                <span class="scrollTitle">Gregory Kelly</span>
-                <span class="scrollInfo position">#30</span><br/>
-                <span class="scrollTitle">QWERTYT</span>
+            <div class="scrollItem" id="">
+                <span class="scrollTitle">Nada Fox</span>
+                <span class="scrollInfo position">#28</span><br/>
+                <span class="scrollTitle">BREIM</span>
             </div>
-            <div class="scrollItem" id="1100">
-                <span class="scrollTitle">Gregory Kelly</span>
-                <span class="scrollInfo position">#30</span><br/>
-                <span class="scrollTitle">QWERTYT</span>
+            <div class="scrollItem" id="">
+                <span class="scrollTitle">Cornelia Edlin</span>
+                <span class="scrollInfo position">#29</span><br/>
+                <span class="scrollTitle">MYNEB</span>
             </div>
-            <div class="scrollItem" id="1100">
-                <span class="scrollTitle">Gregory Kelly</span>
+            <div class="scrollItem" id="">
+                <span class="scrollTitle">Danica Muff</span>
                 <span class="scrollInfo position">#30</span><br/>
-                <span class="scrollTitle">QWERTYT</span>
+                <span class="scrollTitle">NYQJR</span>
             </div>
-            <div class="scrollItem" id="1100">
-                <span class="scrollTitle">Gregory Kelly</span>
-                <span class="scrollInfo position">#30</span><br/>
-                <span class="scrollTitle">QWERTYT</span>
+            <div class="scrollItem" id="">
+                <span class="scrollTitle">Lilia Lomas</span>
+                <span class="scrollInfo position">#31</span><br/>
+                <span class="scrollTitle">QGFMW</span>
             </div>
-            <div class="scrollItem" id="1100">
-                <span class="scrollTitle">Gregory Kelly</span>
-                <span class="scrollInfo position">#30</span><br/>
-                <span class="scrollTitle">QWERTYT</span>
+            <div class="scrollItem" id="">
+                <span class="scrollTitle">Jeffry Brodie</span>
+                <span class="scrollInfo position">#32</span><br/>
+                <span class="scrollTitle">XBGFZ</span>
             </div>
-            <div class="scrollItem" id="1100">
-                <span class="scrollTitle">Gregory Kelly</span>
-                <span class="scrollInfo position">#30</span><br/>
-                <span class="scrollTitle">QWERTYT</span>
-            </div>
-            <div class="scrollItem" id="1100">
-                <span class="scrollTitle">Gregory Kelly</span>
-                <span class="scrollInfo position">#30</span><br/>
-                <span class="scrollTitle">QWERTYT</span>
-            </div>
-            <div class="scrollItem" id="1100">
-                <span class="scrollTitle">Gregory Kelly</span>
-                <span class="scrollInfo position">#30</span><br/>
-                <span class="scrollTitle">QWERTYT</span>
-            </div>
-            <div class="scrollItem" id="1100">
-                <span class="scrollTitle">Gregory Kelly</span>
-                <span class="scrollInfo position">#30</span><br/>
-                <span class="scrollTitle">QWERTYT</span>
+            <div class="scrollItem" id="">
+                <span class="scrollTitle">Cyril Teitelbaum</span>
+                <span class="scrollInfo position">#33</span><br/>
+                <span class="scrollTitle">CJZFM</span>
             </div>
         </div>
         <div id="buttons">
