e3c1936b1b74953a01aa8368e39a759a6d1a380d
Andrew-R-Anderson <19ara@queensu.ca>
2020-03-06 12:01:59 -0500

    Added about section

--- public/index.php
+++ public/index.php
@@ -26,9 +26,19 @@
 
 	<body>
 		<?php include(HEADER); ?>
-		<div style="margin:auto;background-color: #ebebeb;display: block;width:40%;overflow: hidden">
+		<section>
 			<button class="sbutton">This is a button</button>
 			<button class="sbutton">This is a second button</button>
-		</div>
+		</section>
+		
+		<section>
+
+<div class="about-section">
+  <h1>About Us</h1>
+  <p>An issue present to multiple Queen’s students is an inability to access quick healthcare due to the long wait times at the on-campus medical clinic. Qlinic is an app that provides an electronic version of the “take a number” system at clinics. Students can check in to the clinic, see the number of other people ahead of them in line, and see an estimated wait time to see a doctor. Students can also make appointments through Qlinic instead of calling the receptionist.</p>
+    <p><3 Raed ;) -___-</p>
+</div>
+
+		</section>
 	</body>
 </html>
