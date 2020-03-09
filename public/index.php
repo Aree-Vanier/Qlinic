4d464f72b87f5b6830a7a20bab3353f6903bba57
aree-vanier <git@gregk.ca>
2020-03-08 22:50:08 -0400

    Updated homepage buttons

--- public/index.php
+++ public/index.php
@@ -1,34 +1,17 @@
-
 <?php include($_SERVER["DOCUMENT_ROOT"] . "/backend/utils.php") ?>
 <!doctype html>
 <html lang="en">
-	<head>
-		<style>
-			.sbutton { 
-				background-color: #87eaed; 
-				color: black; 
-				padding: 8px; 
-				margin: auto;
-				margin-top: 1em;
-				margin-bottom: 1em;
-				display: block;
-				-webkit-transition-duration: 0.4s; /* Safari */
-				transition-duration: 0.4s;
-			} 
-			.sbutton:hover{
-				background-color: #5d9c9e;
-			}
-		</style>
-		<title>Qlinic Home</title>
-		<?php include(META) ?>
-	</head>
+<head>
+    <title>Qlinic Home</title>
+    <?php include(META) ?>
+</head>
 
 
-	<body>
-		<?php include(HEADER); ?>
-		<div style="margin:auto;background-color: #ebebeb;display: block;width:40%;overflow: hidden">
-			<button class="sbutton">This is a button</button>
-			<button class="sbutton">This is a second button</button>
-		</div>
-	</body>
+<body>
+<?php include(HEADER); ?>
+<section style="display: flex; flex-wrap: wrap; justify-content: center; align-content: space-between; align-items: baseline">
+    <a class="button" href="/queue/join" style="font-size:1.5em; margin-bottom:0.5em">Join Queue</a>
+    <a class="button" href="/appointments/book" style="font-size:1.5em; margin-top:0.5em">Book Appointment</a>
+</section>
+</body>
 </html>
