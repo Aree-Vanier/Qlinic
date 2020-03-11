f669cc567f5a1ac3296f0c02acf535d01bd80c2f
WilliamDormer <williamdormer@gmail.com>
2020-03-11 13:23:47 -0400

    Working on Calendar, in progress.

---
+++ public/RIO/calendar.php
@@ -0,0 +1,18 @@
+<?php include($_SERVER["DOCUMENT_ROOT"] . "/backend/utils.php") ?>
+<?php include($_SERVER["DOCUMENT_ROOT"] . "/backend/appointments.php");
+//$booked = FUNCTION();
+?>
+
+    .calItem {
+        width: 100%;
+        padding: 0px;
+        margin: 0px;
+
+    }
+<h2>Upcoming:</h2>
+
+<?php //For loop to create days and another nested for loop to create appointments ?>
+<div class="day" id="1">
+
+</div> 
+
