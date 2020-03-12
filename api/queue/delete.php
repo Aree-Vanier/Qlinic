b58ab8efd265dfad8655ce56cb473a1950baae67
Aree-Vanier <git@gregk.ca>
2020-03-12 09:04:15 -0400

    Added login check to applicable API endpoints, fixed error when loggedin was not set

--- api/queue/delete.php
+++ api/queue/delete.php
@@ -3,6 +3,11 @@ $headless = true;
 include($_SERVER["DOCUMENT_ROOT"] . "/backend/utils.php");
 include (BACKEND."/queue.php");
 
+session_start();
+if(!(isset($_SESSION['loggedin']) && $_SESSION['loggedin'])){
+    die("ERROR:Invalid login");
+}
+
 $missing = [];
 if(!checkPost(["code","position"], $missing)){
     echo "ERROR:Missing args: ".join(",",$missing);
